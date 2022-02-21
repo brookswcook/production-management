29ff3caf0ebe4e2a927da5eb15936d6f078ef4ee
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-02-21 19:25:27 +0300

    proper layout for data grid

--- packages/dashboard-front-end/src/components/ProductGrid/index.tsx
+++ packages/dashboard-front-end/src/components/ProductGrid/index.tsx
@@ -1,3 +1,4 @@
+import { Container, Grid } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useProductsQuery } from "../../generated/graphql";
 
@@ -6,12 +7,7 @@ export default function ProductGrid() {
     variables: {},
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error.message}</p>;
-  if (data == null || data.products == null)
-    return <p>No products available</p>;
-
-  const rows = data.products;
+  const rows = data ? data.products : [];
   const columns: GridColDef[] = [
     { field: "name", headerName: "Title", width: 150 },
     {
@@ -31,16 +27,21 @@ export default function ProductGrid() {
   ];
 
   return (
-    <div style={{ height: 800, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        getRowId={item => item.name as string}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
-    </div>
+    <Container maxWidth="xl">
+      <Grid item xs={12}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowId={item => item.name as string}
+          pageSize={5}
+          loading={loading}
+          error={error}
+          autoHeight
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </Grid>
+    </Container>
   );
 }

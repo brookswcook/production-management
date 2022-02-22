dc92b44e49a65c5dd742508f0211422b553e9359
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-02-22 15:26:21 +0300

    app toolbar

--- packages/dashboard-front-end/src/components/App/index.tsx
+++ packages/dashboard-front-end/src/components/App/index.tsx
@@ -1,10 +1,27 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import ProductGrid from "../ProductGrid";
-import Title from "../Title";
 
 export default function App() {
   return (
     <div>
-      <Title>Production control app</Title>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Production control app
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
       <ProductGrid />
     </div>
   );

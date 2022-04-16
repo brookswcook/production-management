0ae70dd09d8d6062c08058466b1c7370096d2b92
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-04-16 18:55:23 +0200

    create unique filename when upload

--- packages/dashboard-api/src/modules/style/style.resolver.ts
+++ packages/dashboard-api/src/modules/style/style.resolver.ts
@@ -3,6 +3,7 @@ import { CreateStyleInput, UploadTechPackInput } from "./style.input";
 import { Style, StyleModel } from "./style.model";
 import { upload } from "../../lib/s3";
 import { FileUpload } from "graphql-upload";
+import { extname } from "path";
 
 @Resolver(Style)
 export class StyleResolver {
@@ -24,7 +25,11 @@ export class StyleResolver {
     const styleData: Style = { code, name };
     if (techPack != null) {
       const { file, fileSize } = techPack;
-      styleData.techPackUrl = await this.uploadTechPackFile(file, fileSize);
+      styleData.techPackUrl = await this.uploadTechPackFile(
+        code,
+        file,
+        fileSize
+      );
     }
     return await new StyleModel(styleData).save();
   }
@@ -34,16 +39,19 @@ export class StyleResolver {
   async uploadTechPack(
     @Arg("data") { code, techPack: { file, fileSize } }: UploadTechPackInput
   ): Promise<Style> {
-    const techPackUrl = await this.uploadTechPackFile(file, fileSize);
+    const techPackUrl = await this.uploadTechPackFile(code, file, fileSize);
     return StyleModel.findOneAndUpdateOrFail({ code }, { techPackUrl });
   }
 
   private async uploadTechPackFile(
+    styleCode: string,
     file: Promise<FileUpload>,
     contentLength: number
   ): Promise<string> {
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { filename: fileName, createReadStream } = await file;
+    const { filename, createReadStream } = await file;
+    const extName = extname(filename);
+    const fileName = `tech-pack-${styleCode}-${Date.now()}${extName}`;
     const content = createReadStream();
     return await upload({ fileName, content, contentLength });
   }

e7bcb2fd24996ea5724d56d05ddebbbe7daba0e2
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-04-17 14:50:07 +0200

    updated tech pack uploading form

--- packages/dashboard-front-end/src/components/TechPackParams/index.tsx
+++ packages/dashboard-front-end/src/components/TechPackParams/index.tsx
@@ -1,15 +1,43 @@
-import { Button, Stack, TextField } from "@mui/material";
-import { FormEvent } from "react";
+import { ApolloError } from "@apollo/client";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "react-toastify";
+import { useUploadTechPackMutation } from "../../generated/graphql";
 
 export default function TechPackParams({
-  productCode,
+  styleCode: code,
 }: {
-  productCode?: string;
+  styleCode: string;
 }) {
-  function uploadTechPack(event: FormEvent<HTMLFormElement>) {
+  const [techPackFile, setTechPackFile] = useState<File>();
+  const [uploadTechPackMutation] = useUploadTechPackMutation();
+
+  async function uploadTechPack(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log(data, productCode);
+    try {
+      if (techPackFile) {
+        await uploadTechPackMutation({
+          variables: {
+            data: {
+              code,
+              techPack: { file: techPackFile, fileSize: techPackFile.size },
+            },
+          },
+        });
+      }
+    } catch (error) {
+      toast.error((error as ApolloError).message);
+    }
+  }
+
+  function onTechPackInputChange({
+    target: {
+      files,
+      validity: { valid },
+    },
+  }: ChangeEvent<HTMLInputElement>) {
+    const file = files?.item(0) ?? null;
+    if (valid && file) setTechPackFile(file);
   }
 
   return (
@@ -19,7 +47,17 @@ export default function TechPackParams({
       onSubmit={uploadTechPack}
       spacing={2}
     >
-      <TextField label="Tech pack url" name="techPackUrl" required />
+      <Typography component="span" variant="h6">
+        {`Upload tech pack for style ${code}`}
+      </Typography>
+      <TextField
+        variant="standard"
+        label="Tech Pack"
+        type="file"
+        helperText={`Important: it will override tech pack of all products based on given style`}
+        onChange={onTechPackInputChange}
+        required
+      />
       <Button variant="contained" type="submit">
         Upload
       </Button>

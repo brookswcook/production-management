c8c4bf5c6bc0f0f55824d0a67f07d1a99b1808cc
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-04-19 18:56:42 +0200

    update note model

--- packages/dashboard-api/src/modules/note/note.model.ts
+++ packages/dashboard-api/src/modules/note/note.model.ts
@@ -1,35 +1,52 @@
 import {
   getModelForClass,
+  index,
   ModelOptions,
   prop as Property,
+  ReturnModelType,
 } from "@typegoose/typegoose";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import { Field, ObjectType } from "type-graphql";
 
 @ModelOptions({ schemaOptions: { timestamps: true } })
+@index({ type: 1, parentId: 1 })
 @ObjectType()
 export class Note extends TimeStamps {
   @Field(() => String)
-  @Property()
+  @Property({ required: true })
   parentId!: string;
 
   // TODO: maybe note should not know about given types; create service with needed functions instead
   @Field()
-  @Property()
+  @Property({ required: true })
   type!: "sampleRejectionComment" | "productNote";
 
   @Field()
-  @Property()
+  @Property({ required: true })
   text!: string;
 
-  // @Field()
-  // @Property()
-  // images!: string[];
+  @Field(() => [String])
+  @Property({ default: [] })
+  imageFileNames!: string[];
 
   //TODO: decide
   @Field(() => String, { nullable: true })
   @Property()
   user?: string;
+
+  static async findOneAndUpdateOrFail(
+    this: ReturnModelType<typeof Note>,
+    query: Partial<Note>,
+    update: Partial<Note>
+  ): Promise<Note> {
+    const updatedNote = await this.findOneAndUpdate(
+      query,
+      { $set: update },
+      { returnOriginal: false }
+    ).exec();
+    if (updatedNote == null) throw Error(`Note is not found`);
+    return updatedNote;
+  }
 }
 
 export const NoteModel = getModelForClass(Note);

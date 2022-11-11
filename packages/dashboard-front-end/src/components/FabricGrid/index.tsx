01a6abd8cececb68c8ef18d37d2ce52f9ce5ad61
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-11-11 21:22:35 +0100

    feat(fabric-view): show basic fabric data

--- packages/dashboard-front-end/src/components/FabricGrid/index.tsx
+++ packages/dashboard-front-end/src/components/FabricGrid/index.tsx
@@ -1,12 +1,38 @@
 import { Container, Grid } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { FabricFieldsFragment, useFabricsQuery } from "../../generated/graphql";
 
 export default function FabricGrid() {
+  const { data, loading, error } = useFabricsQuery({});
+  const rows: FabricFieldsFragment[] = data?.fabrics ?? [];
+
+  const columns: GridColDef<FabricFieldsFragment>[] = [
+    {
+      field: "code",
+      headerName: "Code",
+    },
+    {
+      field: "colorName",
+      headerName: "Color Name",
+    },
+  ];
+
   return (
     <Container>
-      <Grid>
-        <DataGrid rows={[]} columns={[]}/>
+      <Grid item xs={12}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowId={item => item.code}
+          pageSize={100}
+          loading={loading}
+          error={error}
+          autoHeight
+          checkboxSelection
+          disableSelectionOnClick
+          sx={{ mt: 1 }}
+        />
       </Grid>
     </Container>
-  )
+  );
 }

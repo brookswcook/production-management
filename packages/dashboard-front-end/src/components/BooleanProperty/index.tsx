7c244a9338cadd0fc056a3ca17498fc047ba78aa
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-11-19 22:52:35 +0100

    feat: add boolean property component

---
+++ packages/dashboard-front-end/src/components/BooleanProperty/index.tsx
@@ -0,0 +1,27 @@
+import CloseIcon from "@mui/icons-material/Close";
+import CheckIcon from "@mui/icons-material/Check";
+import { SxProps, Theme, Typography } from "@mui/material";
+
+export function BooleanProperty({
+  title,
+  value,
+  //fontSize = "small",
+  sx = { pl: 3, textAlign: "center", verticalAlign: "text-bottom" },
+}: {
+  title: string;
+  value: boolean;
+  //fontSize?: "inherit" | "large" | "medium" | "small";
+  sx?: SxProps<Theme>;
+}) {
+  const fontSize = "small";
+  return (
+    <Typography component="span" variant="subtitle2">
+      {title}
+      {value ? (
+        <CheckIcon fontSize={fontSize} sx={sx} />
+      ) : (
+        <CloseIcon fontSize={fontSize} sx={sx} />
+      )}
+    </Typography>
+  );
+}

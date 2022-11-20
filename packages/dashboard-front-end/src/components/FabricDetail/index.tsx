0b9ac1a9a06c84b65be87e383eac0d4e1d3bb0cc
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-11-20 01:05:57 +0100

    feat(fabric-detail): add main section of fabric

---
+++ packages/dashboard-front-end/src/components/FabricDetail/index.tsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  Container,
+  Grid,
+  LinearProgress,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { FabricFieldsFragment, useFabricQuery } from "../../generated/graphql";
+import { BooleanProperty, TextProperty } from "../Properties";
+import { ObjectProperty } from "../Properties/ObjectProperty";
+
+export function FabricDetail() {
+  const { code = "" } = useParams();
+  const { data, error, loading } = useFabricQuery({
+    variables: { code },
+  });
+
+  if (loading)
+    return (
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    );
+  if (data == null || error) return <Fragment>Wrong path!</Fragment>;
+
+  const {
+    title,
+    stage,
+    colorName,
+    colorType,
+    colorCode,
+  }: FabricFieldsFragment = data.fabric;
+
+  return (
+    <Container maxWidth="xl">
+      <Grid
+        container
+        sx={{
+          border: "1px solid #AAAAAA",
+          borderRadius: "5px",
+          m: 1,
+          p: 1,
+        }}
+      >
+        <Grid item xs={12} sx={{ p: 1 }}>
+          <Box sx={{ mb: 2 }}>
+            <Typography component="span" variant="h6">
+              {title?.toUpperCase()}
+            </Typography>
+          </Box>
+          <Stack spacing={3} sx={{ pl: 0.5 }}>
+            <BooleanProperty
+              title="Fabric approved"
+              value={stage === "Approved"}
+            />
+            <TextProperty title="Stage" value={stage} />
+            <Typography component="span" variant="subtitle2">
+              Factory
+            </Typography>
+            <ObjectProperty
+              title="Color"
+              value={{
+                "Color Type": colorType,
+                "Color Name": colorName,
+                "Color Code": colorCode,
+              }}
+            />
+            <Typography component="span" variant="subtitle2">
+              Products
+            </Typography>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}

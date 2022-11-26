28a6d981615bf77da88c072cd5042fd5133ed9ea
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-11-26 21:18:58 +0100

    fix: extend object property component to render boolean props too

--- packages/dashboard-front-end/src/components/Properties/ObjectProperty/index.tsx
+++ packages/dashboard-front-end/src/components/Properties/ObjectProperty/index.tsx
@@ -1,12 +1,31 @@
 import { Grid, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import CheckIcon from "@mui/icons-material/Check";
 
 export function ObjectProperty({
   title,
   value,
 }: {
   title: string;
-  value: Record<string, string>;
+  value: Record<string, string | boolean>;
 }) {
+  const stringValueProperty = (value: string) => (
+    <Typography component="div" variant="overline">
+      {value}
+    </Typography>
+  );
+  const booleanValueProperty = (value: boolean) => {
+    const sx = { pl: 3, textAlign: "center", verticalAlign: "text-bottom" };
+    return (
+      <Typography component="div" variant="overline">
+        {value ? (
+          <CheckIcon fontSize={"small"} sx={sx} />
+        ) : (
+          <CloseIcon fontSize={"small"} sx={sx} />
+        )}
+      </Typography>
+    );
+  };
   return (
     <Grid
       container
@@ -27,14 +46,16 @@ export function ObjectProperty({
           alignItems="baseline"
         >
           {Object.entries(value).map(([key, entryValue]) => {
+            const entryValueType = typeof entryValue;
             return (
               <Grid item sx={{ pl: 3 }} key={key}>
                 <Typography component="div" variant="caption">
                   {key}
                 </Typography>
-                <Typography component="div" variant="overline">
-                  {entryValue}
-                </Typography>
+                {/* TODO: reuse existing string and boolean property components */}
+                {entryValueType === "string"
+                  ? stringValueProperty(entryValue as string)
+                  : booleanValueProperty(entryValue as boolean)}
               </Grid>
             );
           })}

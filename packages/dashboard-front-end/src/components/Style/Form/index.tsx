b6937e355bc865e3808bb95cfe738b6c2cac8b21
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-11-28 12:08:48 +0100

    feat: create style form

---
+++ packages/dashboard-front-end/src/components/Style/Form/index.tsx
@@ -0,0 +1,119 @@
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { FormEvent, Fragment, ReactElement, useState } from "react";
+import { PopperButton } from "../../PopperButton";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  CreateStyleInput,
+  useCreateStyleMutation,
+  useStyleLazyQuery,
+} from "../../../generated/graphql";
+import { toast } from "react-toastify";
+import FilePreload from "../../FilePreload";
+
+export function CreateStyleForm({ footerEl }: { footerEl?: ReactElement }) {
+  const [getStyle] = useStyleLazyQuery();
+  const [newStyleMutation] = useCreateStyleMutation({
+    refetchQueries: ["Styles"],
+  });
+  const [techPackFiles, setTechPackFiles] = useState<File[] | null>(null);
+
+  async function createNewStyle(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const { code, name } = Object.fromEntries(
+      data.entries()
+    ) as unknown as CreateStyleInput;
+
+    try {
+      const { data: styleData } = await getStyle({
+        variables: { code },
+      });
+      if (styleData?.style) {
+        toast.info(
+          `Style with ${code} number already exists. Choose another code`,
+          { delay: 10 }
+        );
+      } else {
+        const newStyleData: CreateStyleInput = {
+          code,
+          name,
+        };
+        if (techPackFiles) {
+          newStyleData.techPack = techPackFiles.map(file => ({
+            file,
+            fileSize: file.size,
+          }));
+        }
+        await newStyleMutation({
+          variables: { data: newStyleData },
+        });
+      }
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  }
+
+  return (
+    <Stack
+      component="form"
+      onSubmit={createNewStyle}
+      spacing={2}
+      autoComplete="off"
+    >
+      <Typography component="h4" variant="inherit">
+        {`Create new Style`}
+      </Typography>
+      <TextField
+        label="Style number"
+        name="code"
+        helperText="Example: VD-193"
+        required
+      />
+      <TextField
+        label="Style name"
+        name="name"
+        helperText="Example: The Mia Dress"
+        required
+      />
+      <FilePreload
+        label="Print file"
+        setFiles={setTechPackFiles}
+        helperText={"You can upload tech pack now or later"}
+        multiple={true}
+      />
+      <Fragment>
+        <Button variant="contained" type="submit">
+          Add Style
+        </Button>
+        {footerEl}
+      </Fragment>
+    </Stack>
+  );
+}
+
+export function CreateStylePopperButton({
+  disabled = false,
+}: {
+  disabled?: boolean;
+}) {
+  const [closeCount, setCloseCount] = useState<number>(0);
+  function cancel() {
+    setCloseCount(closeCount + 1);
+  }
+  return (
+    <PopperButton
+      icon={<AddIcon />}
+      title={"Add Style"}
+      disabled={disabled}
+      popperCloseCounter={closeCount}
+    >
+      <CreateStyleForm
+        footerEl={
+          <Button variant="contained" onClick={cancel}>
+            Cancel
+          </Button>
+        }
+      />
+    </PopperButton>
+  );
+}

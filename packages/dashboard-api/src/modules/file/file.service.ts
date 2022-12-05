97b2e67c8a386d063a3abc19419d24d78d76864e
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-12-06 00:32:35 +0100

    feat: file model

--- packages/dashboard-api/src/modules/file/file.service.ts
+++ packages/dashboard-api/src/modules/file/file.service.ts
@@ -1,45 +1,60 @@
 import { FileUpload } from "graphql-upload";
 import { extname } from "path";
 import { getDownloadLink, upload } from "../../lib/s3";
+import { FileModel, File } from "./file.model";
 import { FileType, IFileUpload } from "./file.types";
 
 export async function uploadFile(
-  parentCode: string,
+  parentId: string,
+  userId: string,
   type: FileType,
   file: Promise<FileUpload>,
   contentLength: number
 ): Promise<string> {
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { filename, createReadStream } = await file;
-  const extName = extname(filename);
-  const fileName = `${type}_${parentCode}_${Date.now()}${extName}`;
+  const { filename: name, createReadStream } = await file;
+  const extName = extname(name);
+  const { uploadingKey: key } = await new FileModel({
+    parentId,
+    type,
+    name,
+    extName,
+    userId,
+  } as Partial<File>).save();
   const content = createReadStream();
-  return await upload({ fileName, content, contentLength });
+  return await upload({ key, content, contentLength });
 }
 
 export async function uploadFiles(
-  parentCode: string,
+  parentId: string,
+  userId: string,
   type: FileType,
   fileUploadInputs: IFileUpload[]
 ) {
-  const fileNames = [];
+  const uploadingKeys = [];
   for await (const fileUploadInput of fileUploadInputs) {
     const { file, fileSize } = fileUploadInput;
-    const uploadedFileName = await uploadFile(parentCode, type, file, fileSize);
-    fileNames.push(uploadedFileName);
+    const uploadingKey = await uploadFile(
+      parentId,
+      userId,
+      type,
+      file,
+      fileSize
+    );
+    uploadingKeys.push(uploadingKey);
   }
-  return fileNames;
+  return uploadingKeys;
 }
 
 // TODO: added for future needs to deal with file folders and etc
-export function getDownloadFileLink(fileName: string): Promise<string> {
-  return getDownloadLink({ fileName });
+export function getDownloadFileLink(key: string): Promise<string> {
+  return getDownloadLink({ key });
 }
 
-export async function getDownloadFileLinks(fileNames: string[]) {
+export async function getDownloadFileLinks(keys: string[]) {
   const fileLinks = [];
-  for await (const fileName of fileNames) {
-    const fileLink = await getDownloadLink({ fileName });
+  for await (const key of keys) {
+    const fileLink = await getDownloadLink({ key });
     fileLinks.push(fileLink);
   }
   return fileLinks;

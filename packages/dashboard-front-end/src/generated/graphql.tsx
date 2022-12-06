e9068299771f7daf774997af5aef13eb852e055f
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-12-06 23:11:01 +0100

    fix: update frontend graphql

--- packages/dashboard-front-end/src/generated/graphql.tsx
+++ packages/dashboard-front-end/src/generated/graphql.tsx
@@ -88,6 +88,7 @@ export type FabricSample = {
 
 export type File = {
   __typename?: 'File';
+  createdAt?: Maybe<Scalars['DateTime']>;
   extName: Scalars['String'];
   id: Scalars['String'];
   name: Scalars['String'];
@@ -439,6 +440,8 @@ export type FabricsQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type FabricsQuery = { __typename?: 'Query', fabrics: Array<{ __typename?: 'Fabric', id: string, code: string, title: string, factoryName: string, colorName: string, colorType: string, colorCode?: string | null, printFileName?: string | null, productCodes: Array<string>, stage: string, notes: Array<{ __typename?: 'Note', id: string, type: string, text: string, imageFileNames: Array<string>, createdAt?: string | null, user?: { __typename?: 'User', firstName: string, fullName: string } | null }>, samples: Array<{ __typename?: 'FabricSample', sku: string, approved?: boolean | null, trackNumber: string, delivered?: boolean | null, note?: { __typename?: 'Note', id: string, type: string, text: string, imageFileNames: Array<string>, createdAt?: string | null, user?: { __typename?: 'User', firstName: string, fullName: string } | null } | null }> }> };
 
+export type FileFieldsFragment = { __typename?: 'File', id: string, name: string, extName: string, uploadingKey: string, createdAt?: string | null, user?: { __typename?: 'User', fullName: string } | null };
+
 export type OperationLogFieldsFragment = { __typename?: 'OperationLog', id: string, name: string, variables: string, createdAt?: string | null, user?: { __typename?: 'User', firstName: string } | null };
 
 export type OperationLogsQueryVariables = Exact<{ [key: string]: never; }>;
@@ -543,14 +546,14 @@ export type StyleQueryVariables = Exact<{
 }>;
 
 
-export type StyleQuery = { __typename?: 'Query', style: { __typename?: 'Style', code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', name: string, extName: string, uploadingKey: string, user?: { __typename?: 'User', fullName: string } | null }> } };
+export type StyleQuery = { __typename?: 'Query', style: { __typename?: 'Style', id: string, code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', id: string, name: string, extName: string, uploadingKey: string, createdAt?: string | null, user?: { __typename?: 'User', fullName: string } | null }> } };
 
 export type StylesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type StylesQuery = { __typename?: 'Query', styles: Array<{ __typename?: 'Style', code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', name: string, extName: string, uploadingKey: string, user?: { __typename?: 'User', fullName: string } | null }> }> };
+export type StylesQuery = { __typename?: 'Query', styles: Array<{ __typename?: 'Style', id: string, code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', id: string, name: string, extName: string, uploadingKey: string, createdAt?: string | null, user?: { __typename?: 'User', fullName: string } | null }> }> };
 
-export type StyleFieldsFragment = { __typename?: 'Style', code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', name: string, extName: string, uploadingKey: string, user?: { __typename?: 'User', fullName: string } | null }> };
+export type StyleFieldsFragment = { __typename?: 'Style', id: string, code: string, name: string, techPackUploaded: boolean, productCodes: Array<string>, techPacks: Array<{ __typename?: 'File', id: string, name: string, extName: string, uploadingKey: string, createdAt?: string | null, user?: { __typename?: 'User', fullName: string } | null }> };
 
 export type CreateStyleMutationVariables = Exact<{
   data: CreateStyleInput;
@@ -721,22 +724,30 @@ export const ProductFieldsFragmentDoc = gql`
     ${FabricSampleFieldsFragmentDoc}
 ${FitSampleFieldsFragmentDoc}
 ${NoteFieldsFragmentDoc}`;
+export const FileFieldsFragmentDoc = gql`
+    fragment fileFields on File {
+  id
+  name
+  extName
+  uploadingKey
+  createdAt
+  user {
+    fullName
+  }
+}
+    `;
 export const StyleFieldsFragmentDoc = gql`
     fragment styleFields on Style {
+  id
   code
   name
   techPackUploaded
   techPacks {
-    name
-    extName
-    uploadingKey
-    user {
-      fullName
-    }
+    ...fileFields
   }
   productCodes
 }
-    `;
+    ${FileFieldsFragmentDoc}`;
 export const UsersDocument = gql`
     query Users {
   users {

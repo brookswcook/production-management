b9140f2d9b7860e51fc0b365f689359604c16d65
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-12-10 23:44:10 +0100

    feat: create user graphql input

--- packages/dashboard-api/src/modules/user/user.input.ts
+++ packages/dashboard-api/src/modules/user/user.input.ts
@@ -1,7 +1,23 @@
+import { UserRole } from "dashboard-core";
 import { Field, InputType } from "type-graphql";
 
 @InputType()
 export class LoginInput {
   @Field()
   token!: string;
+}
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  email!: string;
+
+  @Field()
+  firstName!: string;
+
+  @Field()
+  lastName!: string;
+
+  @Field()
+  role!: UserRole;
 }

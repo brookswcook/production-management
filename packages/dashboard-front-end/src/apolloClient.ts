bf7ad6da60be3833592b1897aeffddee8ed6344f
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-12-16 22:03:56 +0100

    feat: handle 401 properly, closes #25

--- packages/dashboard-front-end/src/apolloClient.ts
+++ packages/dashboard-front-end/src/apolloClient.ts
@@ -2,20 +2,34 @@ import {
   ApolloClient,
   InMemoryCache,
   NormalizedCacheObject,
+  from,
+  ServerError,
 } from "@apollo/client";
 import { config } from "./config";
 import { createUploadLink } from "apollo-upload-client";
+import { onError } from "@apollo/client/link/error";
 
 export default function createClient(
-  authToken: string | null
+  authToken: string | null,
+  resetTokenHandler: VoidFunction
 ): ApolloClient<NormalizedCacheObject> {
+  const terminatingLink = createUploadLink({
+    uri: config.apolloURI,
+    headers: {
+      authorization: authToken ? `Bearer ${authToken}` : "",
+    },
+  });
+
+  const resetTokenLink = onError(({ networkError }) => {
+    if (networkError && networkError.name === "ServerError") {
+      const { statusCode } = networkError as ServerError;
+      if (statusCode === 401) resetTokenHandler();
+      location.reload();
+    }
+  });
+
   return new ApolloClient({
-    link: createUploadLink({
-      uri: config.apolloURI,
-      headers: {
-        authorization: authToken ? `Bearer ${authToken}` : "",
-      },
-    }),
+    link: from([resetTokenLink, terminatingLink]),
     cache: new InMemoryCache(),
   });
 }

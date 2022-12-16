55e0f6f5f528734fa8b3af58c1c0807483aebf70
vitaliyazvinski <vitaliyazvinski@gmail.com>
2022-12-16 13:06:49 +0100

    fix: refactor jwt lib

--- packages/dashboard-core/src/jwt.ts
+++ packages/dashboard-core/src/jwt.ts
@@ -2,9 +2,7 @@ import { sign, decode } from "jsonwebtoken";
 import { DecodedTokenPayload } from "./types";
 
 export function decodeToken<T>(token: string): DecodedTokenPayload<T> | null {
-  const result = decode(token, { json: true });
-  if (result == null) return null;
-  return result as DecodedTokenPayload<T>;
+  return decode(token, { json: true }) as DecodedTokenPayload<T>;
 }
 
 export function signToken<T extends object>(

da030dcf6afc7dc8df1e1182cae9a4241cf5711b
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-01-15 21:57:08 +0100

    feat: add company entity

---
+++ packages/dashboard-api/src/modules/company/company.model.ts
@@ -0,0 +1,38 @@
+import {
+  getModelForClass,
+  ModelOptions,
+  prop as Property,
+} from "@typegoose/typegoose";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { Field, ObjectType } from "type-graphql";
+
+// Note: it should be used as tenant entity
+// TODO: Review all entities to support multitenancy by company
+@ModelOptions({ schemaOptions: { timestamps: true } })
+@ObjectType()
+export class Company implements TimeStamps {
+  @Field()
+  @Property({
+    required: true,
+    unique: true,
+    default(this: Company): string {
+      return this.name.replace(/\W/g, "").toLowerCase();
+    },
+  })
+  // Note: can be used as subdomain and s3Prefix
+  code!: string;
+
+  @Field()
+  @Property({ required: true })
+  name!: string;
+
+  @Field()
+  @Property({ required: true })
+  createdAt!: Date;
+
+  @Field()
+  @Property({ required: true })
+  updatedAt!: Date;
+}
+
+export const CompanyModel = getModelForClass(Company);

77491c14c4ff3dbe38b41b7574cc6b6f4ff4aaf9
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-01-16 21:28:40 +0100

    feat: order item attribute to support variants

--- packages/dashboard-api/src/modules/orderItem/orderItem.model.ts
+++ packages/dashboard-api/src/modules/orderItem/orderItem.model.ts
@@ -5,6 +5,15 @@ import {
 } from "@typegoose/typegoose";
 import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
+class OrderItemAttribute {
+  // TODO: move size type to core types
+  @Field({ nullable: true })
+  @Property()
+  size?: "0-0" | "0" | "2" | "4" | "6" | "8" | "10" | "12" | "14";
+  // Note: extend by adding other attributes if needed; by having array of objects with optional attribute fields it's possible to create any combination of them
+}
+
 @ModelOptions({
   schemaOptions: { collection: "order_items" },
 })
@@ -26,10 +35,9 @@ export class OrderItem {
   @Property({ required: true, default: 1 })
   quantity!: number;
 
-  // TODO: it might be better to introduce variants with these attributes as size and other
-  @Field()
-  @Property({ required: true })
-  size!: number;
+  @Field(() => [OrderItemAttribute])
+  @Property({ type: () => [OrderItemAttribute], required: true, default: [] })
+  variant!: OrderItemAttribute[];
 
   // TODO: decide where to keep the price, maybe it should be a part of a product; or a separate entity with reference to a product
   // Note: it looks like it's production price i.e should be set/calculated by factory

15c00c97601f6e7b8367db98b571013a079eccbd
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-02-13 00:35:47 +0100

    feat(action-logging): action log entity

---
+++ packages/dashboard-api/src/modules/actionLog/actionLog.model.ts
@@ -0,0 +1,47 @@
+import {
+  getModelForClass,
+  ModelOptions,
+  prop as Property,
+} from "@typegoose/typegoose";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { Field, ObjectType } from "type-graphql";
+import { User } from "../user/user.model";
+
+@ModelOptions({
+  schemaOptions: { collection: "action_logs", timestamps: true },
+})
+@ObjectType()
+export class ActionLog extends TimeStamps {
+  @Field()
+  id!: string;
+
+  @Property({ required: true })
+  companyId!: string;
+
+  @Field()
+  @Property({ required: true })
+  userId!: string;
+
+  @Field()
+  @Property({ required: true })
+  entityId!: string;
+
+  @Field()
+  @Property({ required: true })
+  title!: string;
+
+  @Field()
+  @Property({ required: true })
+  entityType!: string;
+
+  @Field(() => User, { nullable: true })
+  @Property({
+    ref: () => User,
+    foreignField: "_id",
+    localField: "userId",
+    justOne: true,
+  })
+  user!: User;
+}
+
+export const ActionLogModel = getModelForClass(ActionLog);

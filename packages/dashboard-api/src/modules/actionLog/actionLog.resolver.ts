92768851260617813188abc788edeb6730039bab
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-02-14 15:01:12 +0100

    feat: be able to get action logs of specific types and ids

--- packages/dashboard-api/src/modules/actionLog/actionLog.resolver.ts
+++ packages/dashboard-api/src/modules/actionLog/actionLog.resolver.ts
@@ -1,5 +1,13 @@
-import { Authorized, FieldResolver, Query, Resolver, Root } from "type-graphql";
+import {
+  Arg,
+  Authorized,
+  FieldResolver,
+  Query,
+  Resolver,
+  Root,
+} from "type-graphql";
 import { TenantId } from "../user/user.decorator";
+import { GetActionLogsInput } from "./actionLog.input";
 import { ActionLog, ActionLogModel } from "./actionLog.model";
 
 @Resolver(ActionLog)
@@ -11,8 +19,23 @@ export class ActionLogResolver {
 
   @Authorized("Admin")
   @Query(() => [ActionLog])
-  actionLogs(@TenantId() companyId: string) {
-    return ActionLogModel.find({ companyId })
+  actionLogs(
+    @TenantId() companyId: string,
+    @Arg("data", { nullable: true }) data: GetActionLogsInput
+  ) {
+    let query:
+      | Record<
+          keyof Pick<ActionLog, "entityType" | "entityId">,
+          { [x: string]: string[] }
+        >
+      | object = {};
+    if (data != null) {
+      query = {
+        entityType: { $in: data.entityTypes },
+        entityId: { $in: data.entityIds },
+      };
+    }
+    return ActionLogModel.find({ ...query, companyId })
       .sort({ _id: -1 })
       .populate("user")
       .exec();

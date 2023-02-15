956219ef5c0f891e3d55e7f8da7e42302a3b8542
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-02-15 11:34:55 +0100

    fix: rearrange plugin order

--- packages/dashboard-api/src/index.ts
+++ packages/dashboard-api/src/index.ts
@@ -42,8 +42,8 @@ class DashboardApolloServer extends Server {
       plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground(),
         ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
-        logPlugin,
         graphqlLoaderPlugin(`${__dirname}/modules/**/*.loader.{js,ts}`),
+        logPlugin,
       ],
       introspection: true,
       schema,

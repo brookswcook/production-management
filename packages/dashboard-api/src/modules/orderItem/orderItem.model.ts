6cb5410ca8a212cc4f08fa8cfc1854bdb71391e1
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-03-18 15:11:53 +0100

    feat: get order items grouped by attribute graphql query

--- packages/dashboard-api/src/modules/orderItem/orderItem.model.ts
+++ packages/dashboard-api/src/modules/orderItem/orderItem.model.ts
@@ -1,5 +1,6 @@
 import {
   getModelForClass,
+  index,
   ModelOptions,
   prop as Property,
 } from "@typegoose/typegoose";
@@ -7,6 +8,7 @@ import { Field, ObjectType } from "type-graphql";
 import { Attribute } from "../attribute/attribute.model";
 import { Product } from "../product/product.model";
 
+@index<OrderItem>({ companyId: 1, orderUid: 1 })
 @ModelOptions({
   schemaOptions: { collection: "order_items" },
 })
@@ -52,20 +54,27 @@ export class OrderItem {
   static async getOrderItemsGroupedByAttributes(
     companyId: string,
     orderUid: number
-  ) {
-    return await OrderItemModel.aggregate<{
-      productCode: string;
-      keys: string[];
-      values: string[];
-      qty: number;
-    }>([
-      { $match: { companyId, orderUid } },
+  ): Promise<OrderItemsGroupedByAttributes[]> {
+    return await OrderItemModel.aggregate<OrderItemsGroupedByAttributes>([
+      {
+        $project: {
+          _id: 0,
+          companyId: 1,
+          orderUid: 1,
+          productCode: 1,
+          variantAttributes: 1,
+          quantity: 1,
+          price: 1,
+        },
+      },
+      { $match: { companyId: companyId, orderUid: orderUid } },
       {
         $group: {
           _id: {
             productCode: "$productCode",
             key: "$variantAttributes.key",
             value: "$variantAttributes.value",
+            unitPrice: "$price",
           },
           qty: { $sum: "$quantity" },
         },
@@ -74,9 +83,14 @@ export class OrderItem {
         $project: {
           _id: 0,
           productCode: "$_id.productCode",
-          keys: "$_id.key",
-          values: "$_id.value",
+          attributes: {
+            $zip: {
+              inputs: ["$_id.key", "$_id.value"],
+            },
+          },
           qty: "$qty",
+          unitPrice: "$_id.unitPrice",
+          extPrice: { $multiply: ["$qty", "$_id.unitPrice"] },
         },
       },
     ]).exec();
@@ -84,3 +98,21 @@ export class OrderItem {
 }
 
 export const OrderItemModel = getModelForClass(OrderItem);
+
+@ObjectType()
+export class OrderItemsGroupedByAttributes {
+  @Field()
+  productCode!: string;
+
+  @Field(() => [[String, String]])
+  attributes!: [string, string][];
+
+  @Field()
+  qty!: number;
+
+  @Field()
+  unitPrice!: number;
+
+  @Field()
+  extPrice!: number;
+}

8aef87d6e17d8a6b897206850b1359b2eb30b465
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-05-10 23:12:46 +0200

    chore: wip object properties as field set

--- packages/dashboard-front-end/src/components/Properties/ObjectProperty/index.tsx
+++ packages/dashboard-front-end/src/components/Properties/ObjectProperty/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { ReactElement } from "react";
 import { BooleanProperty } from "../BooleanProperty";
 import { TextProperty } from "../TextProperty";
@@ -22,39 +22,34 @@ export function ObjectProperty({
   value: Record<string, PropertyValue>;
 }): ReactElement {
   return (
-    <Grid item container columnGap={2} alignItems={"center"}>
-      <Grid item xs={12} sm={"auto"}>
-        <Typography
-          component="span"
-          variant="subtitle2"
-          style={{ whiteSpace: "pre" }}
-        >
-          {title}
-        </Typography>
-      </Grid>
-      <>
-        {Object.entries(value)
-          .filter(([, entryValue]) => entryValue != null)
-          .map(([key, entryValue]) => {
-            const entryValueType = typeof entryValue;
-            return (
-              <Grid item key={key}>
-                <Typography component="div" variant="caption">
-                  {key}
-                </Typography>
-                {entryValueType === "string" || entryValueType === "number" ? (
-                  <TextProperty value={entryValue as string} />
-                ) : entryValueType === "boolean" ? (
-                  <Typography component="div" variant="h5">
-                    <BooleanProperty value={entryValue as boolean} />
+    <Box component="fieldset">
+      <legend>{title}</legend>
+      <Grid item container columnGap={2} alignItems={"center"}>
+        <>
+          {Object.entries(value)
+            .filter(([, entryValue]) => entryValue != null)
+            .map(([key, entryValue]) => {
+              const entryValueType = typeof entryValue;
+              return (
+                <Grid item key={key}>
+                  <Typography component="div" variant="caption">
+                    {key}
                   </Typography>
-                ) : (
-                  <></>
-                )}
-              </Grid>
-            );
-          })}
-      </>
-    </Grid>
+                  {entryValueType === "string" ||
+                  entryValueType === "number" ? (
+                    <TextProperty value={entryValue as string} />
+                  ) : entryValueType === "boolean" ? (
+                    <Typography component="div" variant="h5">
+                      <BooleanProperty value={entryValue as boolean} />
+                    </Typography>
+                  ) : (
+                    <></>
+                  )}
+                </Grid>
+              );
+            })}
+        </>
+      </Grid>
+    </Box>
   );
 }

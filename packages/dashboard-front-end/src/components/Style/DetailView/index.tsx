1938ee0b71ea7a18a00c89cf871906218b524a44
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-05-15 19:41:03 +0200

    fix: update style detail view using new styles

--- packages/dashboard-front-end/src/components/Style/DetailView/index.tsx
+++ packages/dashboard-front-end/src/components/Style/DetailView/index.tsx
@@ -12,49 +12,12 @@ import { BooleanProperty, TextProperty } from "../../Properties";
 import { LinkProperty } from "../../Properties/LinkProperty";
 import { UploadTechPackPopperButton } from "../../TechPackForm";
 
-function StyleHeaderSection({
-  style: { code, name: title, techPackUploaded, productCodes },
+function StyleDetailHeader({
+  style: { name: title, code },
 }: {
   style: StyleFieldsFragment;
 }): ReactElement {
-  return (
-    <>
-      <DetailViewHeaderTitle title={`${title} style`} />
-      <Grid item xs={12} sx={{ pl: 1 }}>
-        <Stack spacing={2} sx={{ pl: 0.5 }}>
-          <TextProperty title="Number" value={code} />
-          <Grid alignItems="baseline" gap={3} container>
-            <Grid item>
-              <BooleanProperty
-                title="Tech pack uploaded"
-                value={techPackUploaded}
-              />
-            </Grid>
-            <RequireRole authorizedRoles={["Admin", "VChapman"]}>
-              <Grid item>
-                <UploadTechPackPopperButton
-                  variant="contained"
-                  styleCode={code}
-                />
-              </Grid>
-            </RequireRole>
-          </Grid>
-          {productCodes.length > 0 ? (
-            <LinkProperty
-              title="Products"
-              baseUrl="products"
-              resources={productCodes.map(code => ({
-                id: code,
-                text: code,
-              }))}
-            />
-          ) : (
-            <TextProperty title="Products" value="no associated products" />
-          )}
-        </Stack>
-      </Grid>
-    </>
-  );
+  return <DetailViewHeaderTitle title={`${title} style #${code}`} />;
 }
 
 export function StyleDetail(): ReactElement {
@@ -72,14 +35,48 @@ export function StyleDetail(): ReactElement {
   if (data == null || error)
     return <>There's no data to show or unexpected error has happened!</>;
 
-  const { id, techPacks } = data.style;
+  const { id, techPacks, techPackUploaded, productCodes } = data.style;
 
   return (
-    <DetailView headerSections={<StyleHeaderSection style={data.style} />}>
-      <DetailViewSection headerTitle="Tech packs:">
+    <DetailView header={<StyleDetailHeader style={data.style} />}>
+      <DetailViewSection>
+        <Grid item xs={12}>
+          <Stack spacing={2}>
+            <Grid alignItems="baseline" gap={3} container>
+              <Grid item>
+                <BooleanProperty
+                  title="Tech pack uploaded"
+                  value={techPackUploaded}
+                />
+              </Grid>
+              <RequireRole authorizedRoles={["Admin", "VChapman"]}>
+                <Grid item>
+                  <UploadTechPackPopperButton
+                    variant="contained"
+                    styleCode={code}
+                  />
+                </Grid>
+              </RequireRole>
+            </Grid>
+            {productCodes.length > 0 ? (
+              <LinkProperty
+                title="Products"
+                baseUrl="products"
+                resources={productCodes.map(code => ({
+                  id: code,
+                  text: code,
+                }))}
+              />
+            ) : (
+              <TextProperty title="Products" value="no associated products" />
+            )}
+          </Stack>
+        </Grid>
+      </DetailViewSection>
+      <DetailViewSection title="tech packs">
         <FileGrid fileType="tech-pack" parentID={id} files={techPacks} />
       </DetailViewSection>
-      <DetailViewSection headerTitle="Timeline">
+      <DetailViewSection title="timeline">
         <EntityTimeline
           entityIds={[id]}
           entityTypes={["Style"]}

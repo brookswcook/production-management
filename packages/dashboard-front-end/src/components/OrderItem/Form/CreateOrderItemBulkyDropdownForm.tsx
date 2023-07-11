1d6d1cdc14cdf23f2dece117cdfc91a45b77a20a
vitaliyazvinski <vitaliyazvinski@gmail.com>
2023-07-11 20:35:34 +0200

    fix: order item price update on mobile devices

--- packages/dashboard-front-end/src/components/OrderItem/Form/CreateOrderItemBulkyDropdownForm.tsx
+++ packages/dashboard-front-end/src/components/OrderItem/Form/CreateOrderItemBulkyDropdownForm.tsx
@@ -5,6 +5,7 @@ import { ProductFieldsFragment } from "../../../generated/graphql";
 import { ProductDropDown } from "../../Product";
 import { OrderItemBulkyFormType } from "./types";
 import VariantAttributeSetDropDown from "../../VariantAttribute/VariantAttributeSetDropDown";
+import { VariantAttributeSet } from "../../VariantAttribute";
 
 export default function CreateOrderItemBulkyDropdownForm({
   onSubmit,
@@ -21,6 +22,9 @@ export default function CreateOrderItemBulkyDropdownForm({
   }>({ itemPrice: 0, discountPerItem: 0 });
   const [product, setProduct] = useState<ProductFieldsFragment | null>(null);
   const [totalQuantity, setTotalQuantity] = useState<number>(0);
+  const [variantAttributeSets, setVariantAttributeSets] = useState<
+    VariantAttributeSet[]
+  >([]);
 
   return (
     <Grid container component="form" id={id} onSubmit={onSubmit} rowGap={1}>
@@ -31,7 +35,14 @@ export default function CreateOrderItemBulkyDropdownForm({
         <>
           <Grid item xs={12}>
             <OrderItemPriceTextField
-              onChange={setPriceDetailsPerItem}
+              onChange={value => {
+                onChange({
+                  productCode: product.code,
+                  pricePerItem: value.itemPrice,
+                  variantAttributeSets: variantAttributeSets,
+                });
+                setPriceDetailsPerItem(value);
+              }}
               product={product}
               quantity={totalQuantity}
             />
@@ -45,6 +56,7 @@ export default function CreateOrderItemBulkyDropdownForm({
                   pricePerItem: priceDetailsPerItem.itemPrice,
                   variantAttributeSets,
                 });
+                setVariantAttributeSets(variantAttributeSets);
                 setTotalQuantity(
                   variantAttributeSets.reduce<number>(
                     (acc, { quantity }) => acc + quantity,
